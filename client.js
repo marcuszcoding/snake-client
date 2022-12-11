3820d8d7448d9b6a8c89a936a92102ded021582c
marcuszcoding <marcuszcoding@gmail.com>
2022-12-11 15:20:44 -0700

    fixed name to write to server not just console.log name in terminal

--- client.js
+++ client.js
@@ -9,7 +9,7 @@ const connect = function () {
 
   conn.on("connect", (connect) => {
     console.log("Connected to game")
-    console.log("Name: MKZ")
+    conn.write("Name: MKZ")
   });
 
   conn.on("data", (data) => {
