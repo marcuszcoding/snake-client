a767b16ebb8a851193a730a7bf458ccd2c75f02f
marcuszcoding <marcuszcoding@gmail.com>
2022-12-11 16:20:38 -0700

    refactored and imported new imput and client

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
-const net = require("net");
+const connect = require("./client");
+const setupInput = require("./input");
 
-const connect = require('./client')
+console.log("Connecting ...");
+connect();
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function () {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+setupInput();
