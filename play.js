8f9068e05ea0b4c359c5f995247a67d0af046f2e
marcuszcoding <marcuszcoding@gmail.com>
2022-12-11 16:07:06 -0700

    added stin and handleUserInput with control c exit

--- play.js
+++ play.js
@@ -1,3 +1,20 @@
 const net = require("net");
 
 const connect = require('./client')
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
