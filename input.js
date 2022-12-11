e40e9f1487460d010b8bce8a44176b28a8a101f5
marcuszcoding <marcuszcoding@gmail.com>
2022-12-11 16:43:14 -0700

    added key inputs and proper connection to each key so the snake moves server side

--- input.js
+++ input.js
@@ -1,4 +1,9 @@
-const setupInput = function () {
+const { connect } = require("./client");
+
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,6 +16,23 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
+
 };
 
 module.exports = setupInput;
