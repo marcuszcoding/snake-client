791f7d9f6b3b26253fcd26cbb9679290b7ff0bd0
marcuszcoding <marcuszcoding@gmail.com>
2022-12-11 19:54:41 -0700

    added comments on keys

--- input.js
+++ input.js
@@ -13,28 +13,28 @@ const setupInput = function(conn) {
 };
 
 const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
+  if (key === '\u0003') { 
+    process.exit(); // if user presses control + c it exits the program
   }
 
   if (key === 'w') {
-    connection.write("Move: up");
+    connection.write("Move: up"); // moves snake upwards with the input of w
   }
 
   if (key === 'a') {
-    connection.write("Move: left");
+    connection.write("Move: left"); // moves snake to the left with input a
   }
 
   if (key === 's') {
-    connection.write("Move: down");
+    connection.write("Move: down"); // moves snake downwards with input s
   }
 
   if (key === 'd') {
-    connection.write("Move: right");
+    connection.write("Move: right"); // moves snake to the right with input d
   }
 
   if (key === 'r') {
-    connection.write("Say: Wassup");
+    connection.write("Say: Wassup"); // input to say "Wassup" to other players with letter r
   }
 
 };
