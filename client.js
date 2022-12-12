0bbd4e7197f744dbf17c8f2cd3280efe4879204d
marcuszcoding <marcuszcoding@gmail.com>
2022-12-11 19:58:44 -0700

    deleted commented out code from a previous exercise

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const net = require("net");
-const { IP, PORT } = require("./constants");
+const { IP, PORT } = require("./constants"); // importing IP and port variables from constants file
 
 // establishes a connection with the game server
 const connect = function() {
@@ -9,11 +9,8 @@ const connect = function() {
   });
 
   conn.on("connect", (connect) => {
-    console.log("Connected to game");
-    conn.write("Name: MKZ");
-    // setTimeout( () => {
-    //   conn.write(`Move: up`)
-    // }, 2000)
+    console.log("Connected to game"); // logs connected string to console to show we are connected to the server
+    conn.write("Name: MKZ"); // initials
   });
 
   conn.on("data", (data) => {
